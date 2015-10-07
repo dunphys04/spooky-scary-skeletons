98b4b62cf132199357c8ed5999d67d692353016f
dunphys04 <dunphys04@bbhcsd.org>
2015-10-07 15:06:09 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -17,6 +17,7 @@ int main()
 		wcout << L"Welcome to spooktacular program of the year!" << endl;
 		wcout << L"You options are as follows:" << endl;
 		wcout << L"1) Open the spook function." << endl;
+
 		wcout << L"2) Open the skelecounter function." << endl;
 		wcout << L"3) Exit." << endl;
 		cin >> option;
@@ -30,7 +31,7 @@ int main()
 			cin >> word;
 			if (word == "skeletons" || word == "skeleton")
 			{
-				while (!kbhit()) //included from conio.h 
+				while (!_kbhit())
 				{
 					system("cls");
 					sleep_for(milliseconds(50));
@@ -70,7 +71,7 @@ int main()
 					wcout << L"░░█░░░░░░░░█░░░░░░░" << endl;
 					wcout << L"░█░░░░░░░░░▌▐░░░░░░" << endl;
 					sleep_for(milliseconds(125));
-			}
+				}
 			}
 			else
 			{
@@ -80,52 +81,60 @@ int main()
 		if (option == 2)
 		{
 			wcout << L"Get ready to have me count some skeletors." << endl;
-			wcout << L"Press any key to start the function." << endl;
-			do //add cin
+			wcout << L"Press enter to start the function." << endl;
+			wcout << L"End the program using space. " << endl;
+			
+			while (1)
 			{
+				if (GetAsyncKeyState(VK_SPACE))
+				{
+					break;
+				}
+
+				using namespace std::this_thread;
+				using namespace std::chrono;
 				int count = 0;
 				system("cls");
-					sleep_for(milliseconds(50));
-					wcout << L"▒▒▒░░░░░░░░░░▄▐░░░░" << endl;
-					wcout << L"▒░░░░░░▄▄▄░░▄██▄░░░ " << endl;
-					wcout << L"░░░░░░▐▀█▀▌░░░░▀█▄░" << endl;
-					wcout << L"░░░░░░▐█▄█▌░░░░░░▀█▄" << endl;
-					wcout << L"░░░░░░░▀▄▀░░░▄▄▄▄▄▀▀" << endl;
-					wcout << L"░░░░░▄▄▄██▀▀▀▀░░░░░" << endl;
-					wcout << L"░░░░█▀▄▄▄█░▀▀░░░░░░" << endl;
-					wcout << L"░░░░▌░▄▄▄▐▌▀▀▀░░░░░" << endl;
-					wcout << L"░▄░▐░░░▄▄░█░▀▀░░░░░" << endl;
-					wcout << L"░▀█▌░░░▄░▀█▀░▀░░░░░" << endl;
-					wcout << L"░░░░░░░░▄▄▐▌▄▄░░░░░" << endl;
-					wcout << L"░░░░░░░░▀███▀█░▄░░░" << endl;
-					wcout << L"░░░░░░░▐▌▀▄▀▄▀▐▄░░░" << endl;
-					wcout << L"░░░░░░░▐▀░░░░░░▐▌░░" << endl;
-					wcout << L"░░░░░░░█░░░░░░░░█░░" << endl;
-					wcout << L"░░░░░░▐▌░░░░░░░░░█░" << endl;
-					sleep_for(milliseconds(125));
-					system("cls");
-					sleep_for(milliseconds(50));
-					wcout << L"░░░░▐▄░░░░░░░░░░▒▒▒" << endl;
-					wcout << L"░░░▄██▄░░▄▄▄░░░░░░▒ " << endl;
-					wcout << L"░▄█▀░░░░▌▀█▀▐░░░░░░" << endl;
-					wcout << L"█▀░░░░░░▌█▄█▐░░░░░░" << endl;
-					wcout << L"▀▄▄▄▄▄░░░▀▄▀░░░░░░░" << endl;
-					wcout << L"░░░░░▀▀▀▀██▄▄▄░░░░░" << endl;
-					wcout << L"░░░░░░▀▀░█▄▄▄▀█░░░░" << endl;
-					wcout << L"░░░░░▀▀▀▌▐▄▄▄░▌░░░░" << endl;
-					wcout << L"░░░░░▀▀░█░▄▄░░░▐░▄░" << endl;
-					wcout << L"░░░░░▀░▀█▀░▄░░░▌█▀░" << endl;
-					wcout << L"░░░░░▄▄▌▐▄▄░░░░░░░░" << endl;
-					wcout << L"░░░▄░█▀███▀░░░░░░░░" << endl;
-					wcout << L"░░░▄▐▀▄▀▄▀▌▐░░░░░░░" << endl;
-					wcout << L"░░▌▐░░░░░░▀▐░░░░░░░" << endl;
-					wcout << L"░░█░░░░░░░░█░░░░░░░" << endl;
-					wcout << L"░█░░░░░░░░░▌▐░░░░░░" << endl;
-					sleep_for(milliseconds(125));
-					(count++);
-			}while (!kbhit()) //escape using space
-				//display counter
+				sleep_for(milliseconds(50));
+				wcout << L"▒▒▒░░░░░░░░░░▄▐░░░░" << endl;
+				wcout << L"▒░░░░░░▄▄▄░░▄██▄░░░ " << endl;
+				wcout << L"░░░░░░▐▀█▀▌░░░░▀█▄░" << endl;
+				wcout << L"░░░░░░▐█▄█▌░░░░░░▀█▄" << endl;
+				wcout << L"░░░░░░░▀▄▀░░░▄▄▄▄▄▀▀" << endl;
+				wcout << L"░░░░░▄▄▄██▀▀▀▀░░░░░" << endl;
+				wcout << L"░░░░█▀▄▄▄█░▀▀░░░░░░" << endl;
+				wcout << L"░░░░▌░▄▄▄▐▌▀▀▀░░░░░" << endl;
+				wcout << L"░▄░▐░░░▄▄░█░▀▀░░░░░" << endl;
+				wcout << L"░▀█▌░░░▄░▀█▀░▀░░░░░" << endl;
+				wcout << L"░░░░░░░░▄▄▐▌▄▄░░░░░" << endl;
+				wcout << L"░░░░░░░░▀███▀█░▄░░░" << endl;
+				wcout << L"░░░░░░░▐▌▀▄▀▄▀▐▄░░░" << endl;
+				wcout << L"░░░░░░░▐▀░░░░░░▐▌░░" << endl;
+				wcout << L"░░░░░░░█░░░░░░░░█░░" << endl;
+				wcout << L"░░░░░░▐▌░░░░░░░░░█░" << endl;
+				sleep_for(milliseconds(125));
+				system("cls");
+				sleep_for(milliseconds(50));
+				wcout << L"░░░░▐▄░░░░░░░░░░▒▒▒" << endl;
+				wcout << L"░░░▄██▄░░▄▄▄░░░░░░▒ " << endl;
+				wcout << L"░▄█▀░░░░▌▀█▀▐░░░░░░" << endl;
+				wcout << L"█▀░░░░░░▌█▄█▐░░░░░░" << endl;
+				wcout << L"▀▄▄▄▄▄░░░▀▄▀░░░░░░░" << endl;
+				wcout << L"░░░░░▀▀▀▀██▄▄▄░░░░░" << endl;
+				wcout << L"░░░░░░▀▀░█▄▄▄▀█░░░░" << endl;
+				wcout << L"░░░░░▀▀▀▌▐▄▄▄░▌░░░░" << endl;
+				wcout << L"░░░░░▀▀░█░▄▄░░░▐░▄░" << endl;
+				wcout << L"░░░░░▀░▀█▀░▄░░░▌█▀░" << endl;
+				wcout << L"░░░░░▄▄▌▐▄▄░░░░░░░░" << endl;
+				wcout << L"░░░▄░█▀███▀░░░░░░░░" << endl;
+				wcout << L"░░░▄▐▀▄▀▄▀▌▐░░░░░░░" << endl;
+				wcout << L"░░▌▐░░░░░░▀▐░░░░░░░" << endl;
+				wcout << L"░░█░░░░░░░░█░░░░░░░" << endl;
+				wcout << L"░█░░░░░░░░░▌▐░░░░░░" << endl;
+				sleep_for(milliseconds(125));
+			}
 		}
-	}while (option != 3);
-	return 0;
+	}
+while (option != 3);
+return 0;
 }
